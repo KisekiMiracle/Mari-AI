f9901988efeb763ca518fa0fd97b0178eeea27de
KisekiMiracle <luisazaldegui99@gmail.com>
2025-04-13 07:39:25 -0500

    small fix

--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,6 @@ import { flatRoutes } from "@react-router/fs-routes";
 export default [
 	route("/", "routes/home.tsx"),
 	route("/login", "routes/login.tsx"),
-	route("/mari", "routes/mari_chat.tsx"),
+	route("/chat", "routes/mari_chat.tsx"),
 	...(await flatRoutes({ rootDirectory: "file-routes" })),
 ] satisfies RouteConfig;
